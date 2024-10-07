4c7a8a353734fa0e72a2500ed7379a17aabec9be
Fredy <fredyfuentes98@yahoo.com>
2024-10-07 04:13:08 -0400

    Testbench now working, uploading updated project folder structure.

--- projectFall2022/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-complete -W "\`grep -oE '^[a-zA-Z0-9_.-]+:([^=]|$)' Makefile | sed 's/[^a-zA-Z0-9_.-]*$//'\`" make
-module load modelsim/2021.2
-module load synopsys/2021
--- projectFall2022/synthesis/run_all.tcl
+++
@@ -1,5 +0,0 @@
-redirect -tee ./logs/setup.log {source -echo setup.tcl} 
-redirect -tee ./logs/read.log {source -echo read.tcl} 
-redirect -tee ./logs/Constraints.log {source -echo Constraints.tcl} 
-redirect -tee ./logs/CompileAnalyze.log {source -echo CompileAnalyze.tcl}
-exit
